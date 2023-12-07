28ddac5b23d67fb30630a451b75b20d555115325
Mariana Lopes Teixeira <73389688+MarianaLopesTeixeira@users.noreply.github.com>
2023-12-07 14:11:05 +0000

    LCD 2 String test
    LCD Testing with 2 strings comutating

--- src/lcd/lcd_test/lcd_test.ino
+++ src/lcd/lcd_test/lcd_test.ino
@@ -3,24 +3,30 @@
 
 // Define LCD dimensions (change these according to your LCD)
 #define LCD_COLUMNS 20
-#define LCD_ROWS    2 //Double check this value.
+#define LCD_ROWS    1 //Double check this value.
 
 // Create an LCD object (8bit mode)
-LiquidCrystal lcd(32, 31, 40, 39, 38, 37, 36, 35, 34, 33); // Pins (LCD): RS, EN, D0, D1, D2, D3, D4, D5, D6, D7. 
-                                                    // Pins (TIVA): PD7, PF4, PF2, PF3, PB3, PC4, PC5, PC6, PC7, PD6.
-                                                    //(Match with schematic).
+LiquidCrystal lcd(PE_0, PC_5, PB_4, PB_5, PB_6, PB_7); // Pins (LCD): RS, EN, D0, D1, D2, D3, D4, D5, D6, D7. 
 
-void setup() {
+void setup() 
+{
   // Initialize the LCD
   lcd.begin(LCD_COLUMNS, LCD_ROWS, LCD_5x8DOTS); //default charsize: LCD_5x8DOTS
-
-  // Print a welcome message
-  lcd.print("LCD Test"); //Check if base is HEX.
-  lcd.setCursor(0, 1);
-  lcd.print("Hello, World!");
 }
 
-void loop() {
+void loop() 
+{
   // main code goes here
+  lcd.print("LCD STRING 1"); //Check if base is HEX.
+  lcd.setCursor(0, 1);
+  delay(1000);
+
+  lcd.clear();
+
+  lcd.print("LCD STRING 2");
+  lcd.setCursor(0, 1);
+  delay(1000);
+
+  lcd.clear();
   // For example, you can update the display based on sensor readings or other events
 }

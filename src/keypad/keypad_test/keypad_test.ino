024a72a6ef61b2a09f22457b06d404d96d116229
Manuel Passadouro <manuel.passadouro@edu.ulisboa.pt>
2023-12-07 16:51:35 +0000

    hw_testing_keypad: changed pin configuration, initial testing (partially working).

--- src/keypad/keypad_test/keypad_test.ino
+++ src/keypad/keypad_test/keypad_test.ino
@@ -10,34 +10,45 @@ char keys[ROWS][COLS] = {
   {'A', '0', 'B', 'C'}
 };
 
-byte rowPins[ROWS] = {PE_5, PB_4, PA_5, PA_6}; // connect to the row pinouts of the keypad
-byte colPins[COLS] = {PB_5, PB_0, PB_1, PE_4}; // connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {PB_0, PB_1, PB_2, PB_3}; // connect to the row pinouts of the keypad
+byte colPins[COLS] = {PE_2, PE_3, PE_4, PE_5}; // connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-//unsigned long debounceTime = 50; //(ms) Adjust this value as needed.
+unsigned long debounceTime = 50; //(ms) Adjust this value as needed.
 
 void setup() {
   Serial.begin(9600); //Double check baud rate.
-  attachInterrupt(digitalPinToInterrupt(PE_5), keypadInterrupt, RISING); //Y1
-  attachInterrupt(digitalPinToInterrupt(PB_4), keypadInterrupt, RISING); //Y2
-  attachInterrupt(digitalPinToInterrupt(PA_5), keypadInterrupt, RISING); //Y3
-  attachInterrupt(digitalPinToInterrupt(PA_6), keypadInterrupt, RISING); //Y4
+  attachInterrupt(digitalPinToInterrupt(PB_0), keypadInterrupt, RISING); //Y1
+  attachInterrupt(digitalPinToInterrupt(PB_1), keypadInterrupt, RISING); //Y2
+  attachInterrupt(digitalPinToInterrupt(PB_2), keypadInterrupt, RISING); //Y3
+  attachInterrupt(digitalPinToInterrupt(PB_3), keypadInterrupt, RISING); //Y4
 }
 
 void loop() {
   // Main loop is empty because we are using interrupts
+  //char key = keypad.getKey();
 }
 
 void keypadInterrupt() {
-  //static unsigned long lastDebounceTime = 0;
-  //unsigned long currentTime = millis();
+  static unsigned long lastDebounceTime = 0;
+  unsigned long currentTime = millis();
 
-  //if (currentTime - lastDebounceTime > debounceTime) {
+  char key = keypad.getKey();
+    
+  if (key != NO_KEY) 
+  {
+    Serial.println(key);
+  }
+
+  if (currentTime - lastDebounceTime > debounceTime) 
+  {
     char key = keypad.getKey();
-    if (key != NO_KEY) {
+    
+    if (key != NO_KEY) 
+    {
       Serial.println(key);
     }
-    //lastDebounceTime = currentTime;
-  //}
+    lastDebounceTime = currentTime;
+  }
 }
